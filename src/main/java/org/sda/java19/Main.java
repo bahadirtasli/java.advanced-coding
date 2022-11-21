50f2ca2c8c27d59b4e3df6415e69c03a1c92e818
Bahadır <m.bahadirtasli@gmail.com>
2022-11-21 21:56:50 +0300

    Warehouse exercise added.

--- src/main/java/org/sda/java19/Main.java
+++ src/main/java/org/sda/java19/Main.java
@@ -1,8 +1,30 @@
 package org.sda.java19;
 
+/**
+ *
+ * 2. Warehouse
+ * a. User should be able to: add, display all of the details, update, delete an item
+ * b. Use composition and collections (The warehouse has products/items)
+ * c. Add possibility to display summaries, like sum of all of the products, their prices.
+ * d. *Add possibility to update number of items in a specific way, e.g.:
+ * “pliers:30”
+ * “scissors:+4”
+ *
+ */
+
 public class Main {
 
     public static void main(String[] args) {
+
+        /*
+        1. Create Warehouse, Product, ProductCategory (enum), Currency (enum)
+        2. Warehouse : name, List of Product, address, isActive
+        3. Product : name, price, quantity, product category, currency, isAvailable
+        4. Create Warehouse object, Before create list of products.
+        5. In main methods, switch case for various operations.
+        6. For each operation, call service method and do the operation.
+
+         */
 
 
     }
